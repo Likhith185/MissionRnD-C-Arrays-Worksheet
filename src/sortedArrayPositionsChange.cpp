0fe087515e66cf8ea8d28aa46409f1c006e94872
Likhith185 <Likhith185@missionrnd.com>
2016-03-12 13:29:24 +0530

    Submit Commit

--- src/sortedArrayPositionsChange.cpp
+++ src/sortedArrayPositionsChange.cpp
@@ -12,8 +12,47 @@ NOTES:
 */
 
 #include <stdio.h>
+/*l=low
+  m=mid
+  h=high*/
+void merge(int a[], int l, int m, int h)
+{
+	int b[1000];
+	int i = l, j = m + 1, k = 0;
 
+	while (i <= m && j <= h) {
+		if (a[i] <= a[j])
+			b[k++] = a[i++];
+		else
+			b[k++] = a[j++];
+	}
+	while (i <= m)
+		b[k++] = a[i++];
+
+	while (j <= h)
+		b[k++] = a[j++];
+
+	k--;
+	while (k >= 0) {
+		a[l + k] = b[k];
+		k--;
+	}
+}
+void sort(int a[], int l, int h)
+{
+	if (l < h) {
+		int m = (h + l) / 2;
+		sort(a, l, m);
+		sort(a, m + 1, h);
+		merge(a, l, m, h);
+	}
+}
 void * sortedArrayPositionsChange(int *Arr, int len)
 {
-	return NULL;
+	if (len < 1 || len == 1 || Arr == NULL)
+		return NULL;
+	else
+	{
+		sort(Arr, 0, len - 1);
+	}
 }
